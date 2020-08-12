382bb22c91f123b8a1d0a62e1718795f0b5170e0
Alex0007 <Alex0007lolpvp@gmail.com>
2020-08-12 15:05:03 +0500

    use single generate task

---
+++ generate.js
@@ -0,0 +1,20 @@
+const OpenAPI = require('openapi-typescript-codegen');
+const fetch = require('node-fetch')
+
+const packageJson = require('./package.json')
+const { preprocessSchema } = require('./preprocess');
+
+const schemaUrl = `https://api.webdock.io/api-docs/v1/webdock-api-v1.yaml`
+
+fetch(schemaUrl).then(async response => {
+    return OpenAPI.generate({
+        input: preprocessSchema(await response.text()),
+        output: './generated',
+        globalHeaders: {
+            "X-Client": `webdock-nodejs-sdk/${packageJson.version}`
+        },
+        httpClient: 'node-fetch' 
+    })
+}).then(() => {
+    console.log('Client generated successfully');
+})
--- preprocess.js
+++ preprocess.js
@@ -1,32 +1,34 @@
 const yaml = require('js-yaml')
-const fs = require('fs')
 const { mapValues } = require('lodash')
 
-const openApiSchema = yaml.safeLoad(fs.readFileSync('./webdock.yaml'));
+exports.preprocessSchema = (yamlSchema) => {
+    const openApiSchema = yaml.safeLoad(yamlSchema);
 
-const requestBodies = openApiSchema.components.requestBodies;
+    const requestBodies = openApiSchema.components.requestBodies;
+    
+    const replaceRefs = (input) => {
+        const iterator = (value) => {
+            if (Array.isArray(value)) {
+                return value.map(value => iterator(value))
+            }
+    
+            if (typeof value === 'object' && value && /requestBodies/.test(value['$ref'])) {
+                const modelName = value['$ref'].split('/').pop()
+                return requestBodies[modelName]
+            }
+    
+            if (typeof value === 'object' && value !== null) {
+                return mapValues(value, iterator)
+            }
+    
+            return value
+        };
+    
+        return mapValues(input, iterator)
+    }
+    
+    const output = replaceRefs(openApiSchema)
 
-const replaceRefs = (input) => {
-    const iterator = (value) => {
-        if (Array.isArray(value)) {
-            return value.map(value => iterator(value))
-        }
-
-        if (typeof value === 'object' && /requestBodies/.test(value['$ref'])) {
-            const modelName = value['$ref'].split('/').pop()
-            return requestBodies[modelName]
-        }
-
-        if (typeof value === 'object') {
-            return mapValues(value, iterator)
-        }
-
-        return value
-    };
-
-    return mapValues(input, iterator)
+    return output;
 }
 
-const output = replaceRefs(openApiSchema)
-
-fs.writeFileSync('webdock.json', JSON.stringify(output))
