ec6ce9bd7c9584fb47419a29fefa4817119bca5c
Alex0007 <Alex0007lolpvp@gmail.com>
2020-08-17 02:15:24 +0500

    init tests

---
+++ test/client.test.js
@@ -0,0 +1,35 @@
+const { OpenAPI } = require("../dist");
+const WebdockApi = require("../dist/services");
+
+// can be set in .env file at the package root
+OpenAPI.TOKEN = process.env.WEBDOCK_TOKEN;
+
+test("Ensure token is set", () => {
+    expect(OpenAPI.TOKEN).toBeTruthy();
+});
+
+test("Ping", async () => {
+    const pingResponse = await WebdockApi.PingService.getPing();
+    expect(pingResponse).toEqual({ webdock: "rocks" });
+});
+
+test("ServersService", async () => {
+    const serversResponse = await WebdockApi.ServersService.getServers();
+    expect(Array.isArray(serversResponse)).toBe(true);
+
+    const slug = serversResponse[0].slug;
+    const singleServerResponse = await WebdockApi.ServersService.getServerBySlug(
+        slug
+    );
+    expect(singleServerResponse.slug).toBe(slug);
+
+    const randomName = `Random name ${parseInt(Math.random() * 10)}`;
+    const patchServerResponse = await WebdockApi.ServersService.patchServer(
+        slug,
+        {
+            ...singleServerResponse,
+            name: randomName,
+        }
+    );
+    expect(patchServerResponse.name).toBe(randomName);
+});
