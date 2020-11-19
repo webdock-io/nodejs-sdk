a1e730445e3a2767a3a14d1b79105cf40b4abb0e
Alex0007 <Alex0007lolpvp@gmail.com>
2020-11-19 05:00:06 +0500

    remove globalHeaders until it's supported in original lib

--- generate.js
+++ generate.js
@@ -1,20 +1,22 @@
-const OpenAPI = require('openapi-typescript-codegen');
-const fetch = require('node-fetch')
+const OpenAPI = require("openapi-typescript-codegen");
+const fetch = require("node-fetch");
 
-const packageJson = require('./package.json')
-const { preprocessSchema } = require('./preprocess');
+const packageJson = require("./package.json");
+const { preprocessSchema } = require("./preprocess");
 
-const schemaUrl = `https://api.webdock.io/api-docs/v1/webdock-api-v1.yaml`
+const schemaUrl = `https://api.webdock.io/api-docs/v1/webdock-api-v1.yaml`;
 
-fetch(schemaUrl).then(async response => {
-    return OpenAPI.generate({
-        input: preprocessSchema(await response.text()),
-        output: './generated',
-        globalHeaders: {
-            "X-Client": `webdock-nodejs-sdk/${packageJson.version}`
-        },
-        httpClient: 'node-fetch' 
+fetch(schemaUrl)
+    .then(async (response) => {
+        return OpenAPI.generate({
+            input: preprocessSchema(await response.text()),
+            output: "./generated",
+            // globalHeaders: {
+            //     "X-Client": `webdock-nodejs-sdk/${packageJson.version}`
+            // },
+            httpClient: "node",
+        });
     })
-}).then(() => {
-    console.log('Client generated successfully');
-})
+    .then(() => {
+        console.log("Client generated successfully");
+    });
