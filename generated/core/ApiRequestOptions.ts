cdae0dceccd83f380cca3b0ee982ce6bb147c6c0
Ahmad Addel <a4amado@gmail.com>
2025-02-17 18:21:42 +0200

    Update ApiRequestOptions.ts

--- generated/core/ApiRequestOptions.ts
+++ generated/core/ApiRequestOptions.ts
@@ -1,16 +1,25 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export type ApiRequestOptions = {
-    readonly method: 'GET' | 'PUT' | 'POST' | 'DELETE' | 'OPTIONS' | 'HEAD' | 'PATCH';
+type BaseApiRequestOptions = {
     readonly url: string;
     readonly path?: Record<string, any>;
     readonly cookies?: Record<string, any>;
     readonly headers?: Record<string, any>;
     readonly query?: Record<string, any>;
-    readonly formData?: Record<string, any>;
-    readonly body?: any;
     readonly mediaType?: string;
     readonly responseHeader?: string;
     readonly errors?: Record<number, string>;
 };
+
+type ApiRequestOptionsWithoutFormDataAndBody = BaseApiRequestOptions & {
+    readonly method: 'GET' | 'HEAD' | 'OPTIONS';
+};
+
+type ApiRequestOptionsWithFormDataAndBody = BaseApiRequestOptions & {
+    readonly method: 'PUT' | 'POST' | 'DELETE' | 'PATCH';
+    readonly formData?: Record<string, any>;
+    readonly body?: any;
+};
+
+export type ApiRequestOptions = ApiRequestOptionsWithoutFormDataAndBody | ApiRequestOptionsWithFormDataAndBody;
